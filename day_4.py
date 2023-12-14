5c4f75ff6105d60a1a83ddabde523711825ff01a
ZduBart <zdubart@zdubart.local>
2023-12-14 15:32:18 +0100

    Day 4 Part 2 Completed

--- day_4.py
+++ day_4.py
@@ -1,12 +1,20 @@
 cards = list(open("day_4_input.txt"))
-score = 0
-for card in cards:
-    winning_numbers = [int(c) for c in card.split("|")[0].split(" ") if c.isdigit()]
-    my_numbers = [int(c) for c in card.split("|")[1].strip().split(" ") if c.isdigit()]
-    power = 0
-    for n in my_numbers:
-        if n in winning_numbers:
-            power += 1
-    if power:
-        score += 2 ** (power - 1)
-print(score)
+multiplier = [1 for _ in cards]
+count = 0
+
+for i, card in enumerate(cards):
+    winning_numbers, my_numbers = card.split("|")
+    winning_numbers = set(
+        int(x) for x in winning_numbers.strip().split() if x.isdigit()
+    )
+    my_numbers = set(int(x) for x in my_numbers.strip().split() if x.isdigit())
+
+    wins = winning_numbers.intersection(my_numbers)
+
+    cmultiplier = multiplier[i]
+    for j in range(i + 1, min(i + len(wins) + 1, len(cards))):
+        multiplier[j] += cmultiplier
+
+    count += cmultiplier
+
+print(count)
