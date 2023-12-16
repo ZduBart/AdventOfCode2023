8e52b711994edf6381a4b64cc3780932f3c4fc96
ZduBart <zdubart@zdubart.local>
2023-12-16 15:15:37 +0100

    Day 8 Part 1 and 2 Completed

---
+++ day_8.py
@@ -0,0 +1,18 @@
+import math, re
+
+dirs, _, *graph = open("day_8_input.txt").read().split("\n")
+graph = {n: d for n, *d in [re.findall(r"\w+", s) for s in graph]}
+print(graph)
+start = [n for n in graph if n.endswith("A")]
+print(start)
+
+
+def solve(pos, i=0):
+    while not pos.endswith("Z"):
+        dir = dirs[i % len(dirs)]
+        pos = graph[pos][dir == "R"]
+        i += 1
+    return i
+
+
+print(solve("AAA"), math.lcm(*map(solve, start)))
